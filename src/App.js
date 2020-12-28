6ddfff3ba76a454e577dd6ed7bc0589ed499b599
rianbloger <rianbloger2@gmail.com>
2020-12-28 13:03:08 +0700

    form handling

--- src/App.js
+++ src/App.js
@@ -1,34 +1,59 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-import { wait } from '@testing-library/react'
 
 class App extends Component{
 
-  constructor (){
-    super()
+    constructor() {
+        super();
 
-    this.state = {
-      user: []
+        this.state = {
+            firstname : '',
+            lastname  : '',
+            name      : '',
+        }
     }
-  }
 
-  getUser = async() => {
-    let response = await axios.get('https://jsonplaceholder.typicode.com/users/1')
-    this.setState({
-      user : response.data
-    })
-  }
+    changeHandler = (e) =>{
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
 
-  componentDidMount(){
-    this.getUser()
-  }
+    submitHandler = (e) => {
+        e.preventDefault()
+        this.setState({
+            name: `${this.state.firstname} ${this.state.lastname}`
+        })
+    }
 
   render(){
-   const {user} = this.state
+        const {name} = this.state;
     return (
-      <div>
-        <div className="text-danger">My name is {user.name} </div>
-        <div>My name is {user.username} </div>
+      <div className="p-5">
+          <div className="container">
+              <div className="row">
+                  <div className="col-md-4">
+                      <div className="card">
+                          <div className="card-header">Learn React JS</div>
+                          <div className="card-body">
+                              <form onSubmit={this.submitHandler}>
+                                  <div className="mb-4">
+                                      <label htmlFor="firstname" className="form-label" >Firstname</label>
+                                      <input type="text" value={this.state.firstname} onChange={this.changeHandler} name="firstname" id="firstname" className="form-control"/>
+                                  </div>
+                                  <div className="mb-4">
+                                      <label htmlFor="lastname" className="form-label" >Lastname</label>
+                                      <input type="text" value={this.state.lastname} onChange={this.changeHandler} name="lastname" id="lastname" className="form-control"/>
+                                  </div>
+                                  <button type="submit" className="btn btn-primary btn-block">Show</button>
+                              </form>
+                          </div>
+                          <div className="card-footer">
+                              My name is {name ? name : '. . . .'}
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
     
     )
