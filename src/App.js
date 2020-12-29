a7b88685877d5f70194e0b744e0e132256a3bb35
rianbloger <rianbloger2@gmail.com>
2020-12-29 12:57:51 +0700

    membuat reusable component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
-import React, {useState} from 'react'
+import React from "react";
+import Card from "./components/Card";
 
-function App() {
-    const [firstname, setFirstname] = useState('')
-    const [lastname, setLastname] = useState('')
-    const [name, setName] = useState('')
-
-    const submitHandler = (e) =>{
-        e.preventDefault();
-        setName( `${firstname} ${lastname}`)
-        setFirstname('')
-        setLastname('')
-    }
+function App(){
     return (
-        <div className="p-5">
+        <div className="py-4">
             <div className="container">
                 <div className="row">
                     <div className="col-md-4">
-                        <div className="card">
-                            <div className="card-header">Lewarn React Function</div>
-                            <div className="card-body">
-                                <form onSubmit={submitHandler} >
-                                    <div className="mb-4">
-                                        <label htmlFor="firstname" className="form-label" >Firstname</label>
-                                        <input type="text" value={firstname} onChange={(e)=>setFirstname(e.target.value)} name="firstname" id="firstname" className="form-control"/>
-                                    </div>
-                                    <div className="mb-4">
-                                        <label htmlFor="lastname" className="form-label" >Lastname</label>
-                                        <input type="text" value={lastname} onChange={(e)=>setLastname(e.target.value)} name="lastname" id="lastname" className="form-control"/>
-                                    </div>
-                                    <button type="submit" className="btn btn-primary btn-block">Show</button>
-                                </form>
-                            </div>
-                            <div className="card-footer">
-                                My name is {name ? name : '. . .'}
-                            </div>
-                        </div>
+                        <Card content={{
+                            imageUrl:"http://placekitten.com/g/300/200",
+                            title:"First post",
+                            published:"06 Feb, 2020",
+                        }}
+                        />
                     </div>
+                    <div className="col-md-4">
+                        <Card content={{
+                            imageUrl:"http://placekitten.com/300/200",
+                            title:"Second post",
+                            published:"06 Feb, 2020",
+                        }}
+                        />
+                    </div>
+                    <div className="col-md-4">
+                        <Card content={{
+                            imageUrl:"http://placekitten.com/g/300/200",
+                            title:"Third post",
+                            published:"06 Feb, 2020",
+                        }}
+                        />
+                    </div>
+
                 </div>
             </div>
         </div>
     )
 }
 
-export default App
+export default App;
